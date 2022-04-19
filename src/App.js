8e7e0397be04ccd0346a3a5c8d228ca88ed1d95b
JelenaJelenkovic <57746585+JelenaJelenkovic@users.noreply.github.com>
2022-04-19 20:08:36 +0200

    verzija 1
    Inicijalizacija projekta

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Pocetna from './Pocetna'
+import Opis from './Opis'
+import {Route} from 'react-router-dom'
+import Meni from './Meni'
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="App" style={{backgroundColor:'#ffb399'}}>
+      <Meni />
+      <Route exact path="/" component={Pocetna} />
+      <Route exact path="/Opis" component={Opis} />
+    </div>
+  );
+}
+
+export default App;
